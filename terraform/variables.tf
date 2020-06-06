ddf9b450db29eac2043f0abb3dfad83ddb6cf910
ethanbrooks <ebrooks@hotelherrera.com>
2020-06-06 08:50:17 -0500

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,8 @@
 variable "access_key" {
     type = string
-	default = "AKIAYJF7JKFVTCLTVAMG"
 }
 variable "secret_key" {
     type = string
-	default = "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"
 }
 variable "region" {
     type = string
